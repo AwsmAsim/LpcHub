ba0f95dc4b7fef5b85803360798ec16d3fa8980d
aayeshanomani <aayeshanomani@gmail.com>
2020-08-31 23:54:32 +0530

    pta nhi kya kiya

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -12,66 +12,66 @@ class AvailableFonts {
 class AvailableImages {
 
   static const man1 = {
-    'assetImage' : AssetImage('assets/images/man1.jpg'),
-    'assetPath' : 'assets/images/man1.jpg',
+    'assetImage' : AssetImage('assets/man1.jpg'),
+    'assetPath' : 'assets/man1.jpg',
   };
 
   static const man2 = {
-    'assetImage' : AssetImage('assets/images/man2.jpg'),
-    'assetPath' : 'assets/images/man2.jpg',
+    'assetImage' : AssetImage('assets/man2.jpg'),
+    'assetPath' : 'assets/man2.jpg',
   };
 
   static const man3 = {
-    'assetImage' : AssetImage('assets/images/man3.jpg'),
-    'assetPath' : 'assets/images/man3.jpg',
+    'assetImage' : AssetImage('assets/man3.jpg'),
+    'assetPath' : 'assets/man3.jpg',
   };
 
   static const man4 = {
-    'assetImage' : AssetImage('assets/images/man4.jpg'),
-    'assetPath' : 'assets/images/man4.jpg',
+    'assetImage' : AssetImage('assets/man4.jpg'),
+    'assetPath' : 'assets/man4.jpg',
   };
 
   static const man5 = {
-    'assetImage' : AssetImage('assets/images/man5.jpg'),
-    'assetPath' : 'assets/images/man5.jpg',
+    'assetImage' : AssetImage('assets/man5.jpg'),
+    'assetPath' : 'assets/man5.jpg',
   };
 
 
   static const woman1 = {
-    'assetImage' : AssetImage('assets/images/woman1.jpg'),
-    'assetPath' : 'assets/images/woman1.jpg',
+    'assetImage' : AssetImage('assets/woman1.jpg'),
+    'assetPath' : 'assets/woman1.jpg',
   };
 
   static const woman2 = {
-    'assetImage' : AssetImage('assets/images/woman2.jpg'),
-    'assetPath' : 'assets/images/woman2.jpg',
+    'assetImage' : AssetImage('assets/woman2.jpg'),
+    'assetPath' : 'assets/woman2.jpg',
   };
 
   static const woman3 = {
-    'assetImage' : AssetImage('assets/images/woman3.jpg'),
-    'assetPath' : 'assets/images/woman3.jpg',
+    'assetImage' : AssetImage('assets/woman3.jpg'),
+    'assetPath' : 'assets/woman3.jpg',
   };
 
   static const woman4 = {
-    'assetImage' : AssetImage('assets/images/woman4.jpg'),
-    'assetPath' : 'assets/images/woman4.jpg',
+    'assetImage' : AssetImage('assets/woman4.jpg'),
+    'assetPath' : 'assets/woman4.jpg',
   };
 
   static const woman5 = {
-    'assetImage' : AssetImage('assets/images/woman5.jpg'),
-    'assetPath' : 'assets/images/woman5.jpg',
+    'assetImage' : AssetImage('assets/woman5.jpg'),
+    'assetPath' : 'assets/woman5.jpg',
   };
 
   static const postBanner = {
-    'assetImage' : AssetImage('assets/images/post_banner.jpg'),
-    'assetPath' : 'assets/images/post_banner.jpg',
+    'assetImage' : AssetImage('assets/post_banner.jpg'),
+    'assetPath' : 'assets/post_banner.jpg',
   };
 
   static const emptyState = {
-    'assetImage' : AssetImage('assets/images/empty.png'),
-    'assetPath' : 'assets/images/empty.png',
+    'assetImage' : AssetImage('assets/empty.png'),
+    'assetPath' : 'assets/empty.png',
   };
 
-  static const homePage = const AssetImage('assets/images/home_page.png');
+  static const homePage = const AssetImage('assets/home_page.jpg');
   static const appLogo = const AssetImage('assets/logo_wot.jpg');
 }
